f25e3efaaeccc39a9c2158923c4953f3fb101745
Gautam Nigam <gautamrn@umich.edu>
2024-08-06 22:07:53 -0400

    lyrics showing, facts about song

--- src/components/songs/Lyrics.jsx
+++ src/components/songs/Lyrics.jsx
@@ -1,38 +1,64 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Loading from '../setup/Loading';
+import { Link, useParams } from 'react-router-dom';
+import Moment from 'react-moment';
 
-class Lyrics extends Component {
-    state = {
-        track: {},
-        lyrics: {}
-    };
+function Lyrics() {
+  const { id } = useParams(); // Access the 'id' route parameter
 
-    componentDidMount(){
-        axios.get(
-            `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=
-            ${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`)
-        .then(res=> {
-            this.setState({lyrics: res.data.message.body.lyrics});
+  const [track, setTrack] = useState({});
+  const [lyrics, setLyrics] = useState({});
 
-            return axios.get(
-                `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id=
-                ${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`);
-        })
-        .then(res => {
-            this.setState({track: res.data.message.body.track});
-        })
-        .catch(err => console.log(err));
-    }
+  useEffect(() => {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${id}&apikey=${process.env.REACT_APP_MM_KEY}`
+      )
+      .then((res) => {
+        setLyrics(res.data.message.body.lyrics);
 
+        return axios.get(
+          `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id=${id}&apikey=${process.env.REACT_APP_MM_KEY}`
+        );
+      })
+      .then((res) => {
+        setTrack(res.data.message.body.track);
+      })
+      .catch((err) => console.log(err));
+  }, [id]); // Dependency array with 'id'
 
-  render() {
-    const {lyrics, track} = this.state;
-
-    if(track === undefined || Object.keys(track).length === 0 || lyrics === undefined){
-        return <Loading />
-    }
-
+  if (!track || Object.keys(track).length === 0 || !lyrics || Object.keys(lyrics).length === 0) {
+    return <Loading />;
+  } else {
+    return (
+      <div>
+        <Link to="/" className="btn btn-dark btn-sm mb-4">
+          Back
+        </Link>
+        <div className='card'>
+            <h5 className="card-header">
+            {track.track_name} by <span className="text-secondary">{track.artist_name}</span>
+            </h5>
+            <div className='card-body'>
+                <p className='card-text'>{lyrics.lyrics_body}</p>
+            </div>
+        </div>
+        <ul className='list-group mt-3'>
+            <li className='list-group-item'>
+                <strong>Genre: </strong>{track.primary_genres.music_genre_list[0].music_genre.music_genre_name}
+            </li>
+            <li className='list-group-item'>
+              <strong>Explicit?: </strong>
+              {track.explicit === 0 ? "No" : "Yes"}
+            </li>
+            <li className='list-group-item'>
+              <strong>Release Date: </strong>
+              <Moment format='MM/DD/YYYY'>{track.first_release_date}</Moment>
+            </li>
+        </ul>
+      </div>    
+    );
   }
 }
 
